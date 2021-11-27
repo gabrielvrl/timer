b3720fefd30c8ca69f4809a82b1a59646c0592b7
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2021-11-27 15:52:46 -0300

    Binding Events in a Class

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Hi there!")
+class Timer {
+  constructor(durantionInput, startButton, pauseButton){
+    this.durantionInput = durantionInput;
+    this.startButton = startButton;
+    this.pauseButton = pauseButton;
+
+    this.startButton.addEventListener('click', this.start);
+  }
+
+  start(){
+    console.log('Time to start the timer!')
+  }
+}
+
+const durationInput = document.querySelector('#duration');
+const startButton = document.querySelector('#start');
+const pauseButton = document.querySelector('#pause');
+
+const timer = new Timer(durationInput, startButton, pauseButton);
