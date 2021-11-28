7a288bb58340e8b667a385d192ab233a4374aeee
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2021-11-28 10:59:28 -0300

    Starting and Pausing the Timer

--- index.js
+++ index.js
@@ -5,9 +5,21 @@ class Timer {
     this.pauseButton = pauseButton;
 
     this.startButton.addEventListener('click', this.start);
+    this.pauseButton.addEventListener('click', this.pause);
   }
 
-  start= () => {}
+  start= () => {
+    this.tick();
+    this.interval = setInterval(this.tick, 1000);
+  }
+
+  pause = () => {
+    clearInterval(this.interval)
+  }
+
+  tick = () => {
+    console.log('tick')
+  }
 }
 
 const durationInput = document.querySelector('#duration');
