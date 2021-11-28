26046851557285b57b230e0378e334897ca338f5
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2021-11-28 12:14:16 -0300

    OnTick and OnComplete

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ class Timer {
     this.pauseButton = pauseButton;
     if (callbacks) {
       this.onStart = callbacks.onStart;
+      this.onTick = callbacks.onTick;
+      this.onComplete = callbacks.onComplete;
     }
 
     this.startButton.addEventListener('click', this.start);
@@ -26,8 +28,14 @@ class Timer {
   tick = () => {
     if(this.timeRemaining <= 0) {
       this.pause();
+      if(this.onComplete){
+        this.onComplete();
+      }
     } else {
       this.timeRemaining = this.timeRemaining - 1;
+      if(this.onTick){
+        this.onTick();
+      }
     }
   }
 
@@ -49,9 +57,9 @@ const timer = new Timer(durationInput, startButton, pauseButton, {
     console.log('Timer started')
   },
   onTick() {
-
+    console.log('Timer just ticked down')
   },
   onComplete() {
-
+    console.log('Timer is completed')
   }
 });
