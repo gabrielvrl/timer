df222e0263f3ed35ab6cb31f965e7f00c0a2f7e3
Gabriel Varela <gabrielvarelaneto11@gmail.com>
2021-11-28 13:26:59 -0300

    Using Icons

--- timer.js
+++ timer.js
@@ -18,7 +18,7 @@ class Timer {
       this.onStart(this.timeRemaining);
     }
     this.tick();
-    this.interval = setInterval(this.tick, 50);
+    this.interval = setInterval(this.tick, 20);
   }
 
   pause = () => {
@@ -32,7 +32,7 @@ class Timer {
         this.onComplete();
       }
     } else {
-      this.timeRemaining = this.timeRemaining - 0.05;
+      this.timeRemaining = this.timeRemaining - 0.02;
       if(this.onTick){
         this.onTick(this.timeRemaining);
       }
